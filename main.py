3c593fce871803fb679d3b593116418d5afc537a
Sagar Waghunde <sagar@Sagars-MacBook-Pro.local>
2025-12-09 07:42:29 +0800

    implemented rag_chain using lcel

--- main.py
+++ main.py
@@ -7,7 +7,13 @@ from langchain_core.prompts import PromptTemplate
 from langchain_classic import hub
 from langchain_classic.chains.combine_documents import create_stuff_documents_chain
 from langchain_classic.chains.retrieval import create_retrieval_chain
+from langchain_core.runnables import RunnablePassthrough
+
 load_dotenv()
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
 
 if __name__ == "__main__":
     print("Starting the application...")
@@ -29,5 +35,28 @@ if __name__ == "__main__":
     retrieval_chain = create_retrieval_chain(retriever=vectorstore.as_retriever(), combine_docs_chain=combine_docs_chain)
 
     result = retrieval_chain.invoke(input={"input": query})
+    print("--------------------------------")
+    print(result)
+
+    template = """Use the following pieces of retrieved context to answer the question. 
+    If you don't know the answer, just say that you don't know, don't try to make up an answer.
+    Use three sentences maximum and keep the answer as concise as possible.
+    Always say "Thanks for asking!" at the end of the answer.
+    
+    Context: {context}
+    
+    Question: {question}
+
+    Helpful Answer:
+    """
+
+    custom_rag_prompt = PromptTemplate.from_template(template=template)
+    rag_chain = (
+        {"context": vectorstore.as_retriever() | format_docs, "question": RunnablePassthrough()}
+        | custom_rag_prompt 
+        | llm
+    )
+
+    result = rag_chain.invoke(query)
     print("--------------------------------")
     print(result)
